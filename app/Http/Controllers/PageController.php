dac3f3c6fadae44fd60a430e52838e852970e625
jpeace08 <phamgiau2024@gmail.com>
2020-12-24 13:09:18 +0700

    fix time 2

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,15 +2,10 @@
 
 namespace App\Http\Controllers;
 
-<<<<<<< HEAD
 
 use App\Models\Slider;
-=======
 use App\Models\Category;
->>>>>>> 79f84acc6a14b17bc25e0e2510a199abbe08be39
-=======
-use App\Models\Category;
->>>>>>> 79f84acc6a14b17bc25e0e2510a199abbe08be39
+
 use Illuminate\Http\Request;
 
 class PageController extends Controller
@@ -26,8 +21,5 @@ class PageController extends Controller
         $category_pro = Category::find($id);
         return view('pages.category_product',compact('category_pro'));
     }
-    public function category_product($id){
-        $category_pro = Category::find($id);
-        return view('pages.category_product',compact('category_pro'));
-    }
+
 }
