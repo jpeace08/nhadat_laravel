c44b7adcf915c4a9ed1b1458d63dee9f3155b56b
jpeace08 <phamgiau2024@gmail.com>
2020-12-24 17:17:37 +0700

    fix merge br jp

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,21 +2,11 @@
 
 namespace App\Http\Controllers;
 
-<<<<<<< HEAD
-
 use App\Models\Slider;
 
 use App\Models\Category;
-<<<<<<< HEAD
+use App\Models\Product;
 
-=======
-use App\Models\Slider;
-use App\Models\Category;
-use App\Models\Product;
->>>>>>> jpeace08
-=======
-use App\Models\Product;
->>>>>>> 485727774c84978a4092a4a6f50e9c4a16d520e2
 use Illuminate\Http\Request;
 
 class PageController extends Controller
@@ -38,21 +28,15 @@ class PageController extends Controller
         $product = Product::where('slug',$slug)->first();
         $cate_products= $product->categories->first();
         $related_products = $cate_products->products->take(6);
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-    public function getCategoryProducts(Request $request){
+        return view('pages.detail',compact('product','related_products'));
+    }
+    public function getCategoryProducts(Request $request)
+    {
         $posts = Product::where([
             'category_id' => $request->id,
         ])->orderBy('updated_at', 'desc')->paginate(6);
-        
+
         return json_encode($posts);
     }
 
->>>>>>> jpeace08
-=======
-        return view('pages.detail',compact('product','related_products'));
-    }
->>>>>>> 485727774c84978a4092a4a6f50e9c4a16d520e2
 }
